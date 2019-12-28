f2af2238bf9bac2936e25b98d34266e9b8fda3f5
xdkoooo <421280548@qq.com>
2019-12-28 18:55:30 +0800

    div p add

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function MyHeader() {
-    return <div></div>
+    return <div>
+        <p>test</p>
+    </div>
 }
 
 ReactDOM.render(<MyHeader />, document.getElementById('app'));
