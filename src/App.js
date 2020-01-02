28003840034b092ff24ef8755f4bf736f798ac2d
xdkoooo <421280548@qq.com>
2020-01-02 17:01:30 +0800

    二级路由.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link, NavLink, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NewCenter from './pages/NewsCenter';
+import News from './pages/News';
+import style from './pages/style.css';
 
 class App extends Component {
     constructor(props) {
         super(props)
     }
     render () {
-        return <div>
-            App
-        </div>
+        const { pathname } = this.props.location;
+        return <Fragment>
+            <Link to={'/home'} className={ pathname === '/home' ? style.active : ''}>首页</Link>
+            <Link to={'/about'} className={ pathname === '/about' ? style.active : ''}>关于</Link>
+            <Link to={'/news'} className={ pathname === '/news' ? style.active : ''}>新闻中心</Link>
+            <Link to={'/news/1'} className={ pathname === '/news/1' ? style.active : ''}>新闻1</Link>
+            <Link to={'/news/2'} className={ pathname === '/news/2' ? style.active : ''}>新闻2</Link>
+
+            {/* <NavLink to={'/home'}>首页</NavLink>
+            <NavLink to={'/about'}>关于</NavLink>
+            <NavLink to={'/news'} exact={true}>新闻中心</NavLink>
+            <NavLink to={'/news/1'} exact={true}>新闻1</NavLink>
+            <NavLink to={'/news/2'} exact={true}>新闻2</NavLink> */}
+
+            <Route path={'/home'} component={Home} />
+            <Route path={'/about'} component={About} />
+            <Route path={'/news'} component={NewCenter} exact/>
+            <Route path={'/news/:id'} component={News} />
+        </Fragment>
     }
 }
 export default App
--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Link, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import App from './App';
-import Home from './pages/Home';
-import About from './pages/About';
-import NewCenter from './pages/NewsCenter';
-import News from './pages/News';
 
 
 
 ReactDOM.render(
-    <HashRouter>        
-        <Link to={'/home'}>首页</Link>
-        <Link to={'/about'}>关于</Link>
-        <Link to={'/news'}>新闻中心</Link>
-        <Link to={'/news/1'}>新闻1</Link>
-        <Link to={'/news/2'}>新闻2</Link>
-
-        <Route path={'/home'} component={Home} />
-        <Route path={'/about'} component={About} />
-        <Route path={'/news/:id'} component={News} />
-        <Route path={'/news'} component={NewCenter} exact />      
+    <HashRouter>
+        <Route path={'/'} component={App}></Route>    
     </HashRouter>,
     document.getElementById('app'));
