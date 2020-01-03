74e6d64ad66812ac9ace344715cca4518712d5df
xdkoooo <421280548@qq.com>
2020-01-03 19:04:48 +0800

    list show

--- src/pages/MovieMain.js
+++ src/pages/MovieMain.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Axios from 'axios';
+import { Card, Rate } from 'antd';
+import styles from './MovieMain.less';
+const { Meta } = Card;
 
 class MovieMain extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            list: []
+        }
     }
     async componentWillMount() {
         const { type, page } = this.props.match.params;
        
 
-        // let res = await Axios.get('/api/v2/movie/in_theaters')
-        // console.log(res.data);
+        let res = await Axios.get('/api/v2/movie/in_theaters')
+        console.log(res.data.subjects);
+        this.setState(() => {
+            return {
+                list: res.data.subjects
+            }
+        })
 
-        let data = await fetch('/api/v2/movie/in_theaters')
-        let res = await data.json();
-        console.log(res);
+        // let data = await fetch('/api/v2/movie/in_theaters')
+        // let res = await data.json();
+        // console.log(res.subjects);
     }
 
     componentDidMount() {}
 
+    renderList () {
+        return this.state.list.map(item => (
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src={item.images.large} />}
+            >
+                <Meta title={<Rate allowHalf disabled defaultValue={item.rating.average/2} />} description={item.title} />
+            </Card>
+        ))
+    }
+
     render () {
         const { pathname } = this.props.location;
         const { type, page } = this.props.match.params;
-        return <div>
-            MovieMain { type } { page } { pathname }
+        return <div className={styles.list}>    
+             {this.renderList()}
         </div>
     }
 }
